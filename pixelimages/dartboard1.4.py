ba4ab2ec2b798b27ce26f38dfdc7798d7cba7683
Sean Chan <seanyk.chan@gmail.com>
2025-09-22 16:44:53 +0800

    refractor(files): renamed some files to correct module number

--- pixelimages/dartboard1.4.py
+++ pixelimages/dartboard1.4.py
@@ -3,7 +3,7 @@ from PIL import Image, ImageDraw
 import random
 
 # .open("") - lets computer know which image you will load
-img = Image.open("pixelimages\\dartboard1.png")  # Replace with your file path
+img = Image.open("pixelimages\\bolt.png")  # Replace with your file path
 img = img.convert("RGB")  # Ensures it’s in RGB mode
 
 # load the image into your program's memory.
@@ -15,8 +15,8 @@ img.show()
 # Access pixel values
 width, height = img.size
 
-inside_dart_board = 0
-outside_dart_board = 0
+inside_bolt = 0
+outside_bolt = 0
 # Draw blue dots around the image by editing pixel values to blue.
 for i in range(5000):
 	# Randomly select a number within the range of the width of the image.
@@ -25,12 +25,12 @@ for i in range(5000):
     rand_y = random.randint(0,height-1)
     print(pixels[rand_x,rand_y])
     if pixels[rand_x,rand_y] == (255,255,255):
-        outside_dart_board += 1
+        outside_bolt += 1
         pixels[rand_x,rand_y] = (255,0,0)
     else:
-        inside_dart_board += 1
+        inside_bolt += 1
         pixels[rand_x,rand_y] = (0,0,255)
 
 # Show image with the edits.
 img.show()
-print(f"Percentage space taken by dartboard: {inside_dart_board/(outside_dart_board+inside_dart_board)*100}%")
+print(f"Percentage space taken by bolt: {inside_bolt/(outside_bolt+inside_bolt)*100}%")
